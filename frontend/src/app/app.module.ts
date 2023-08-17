6d2fdfb88f70de68ce1f76ec7156b8509c7c7d55
Shrii939 <shridhars145@gmail.com>
2023-08-17 20:55:33 +0530

    header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/modularElements/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { SupportComponent } from './components/pages/support/support.component';
+import { FooterComponent } from './components/modularElements/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SupportComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
